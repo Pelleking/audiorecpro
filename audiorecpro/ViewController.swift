907cea6eb70b05abb19b300d593e35534846b240
Pelle Fredrikson <pelle.fredrikson@academedia.se>
2023-06-24 22:30:19 +0200

    Initial Commit

---
+++ audiorecpro/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  audiorecpro
+//
+//  Created by Pelle Fredrikson on 2023-06-24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
